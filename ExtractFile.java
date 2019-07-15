1c0521ccc90cbad97e5cc0c1cdf527225ca15813
velizarPecev <pecev_2003@abv.bg>
2019-07-15 21:07:37 +0300

    Java

--- ExtractFile.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class ExtractFile {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        String[] file = scanner.nextLine().split("");
-    }
-}
